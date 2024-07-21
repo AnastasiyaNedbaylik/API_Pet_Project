2bc8dbc7a98b4ec71d941484173cab4a87080731
AnastasiyaNedbaylik <161625036+AnastasiyaNedbaylik@users.noreply.github.com>
2024-07-21 20:42:57 +0300

    Update test_pets.py

--- tests/test_pets.py
+++ tests/test_pets.py
@@ -17,67 +17,67 @@ def test_get_token():
 @pytest.mark.smoke
 def test_register_user():
     status = pet.register_user()
-    assert status == 200, f"Ожидался статус 200, но получен статус {status}"
+    assert status == 200, f"Получен статус {status} вместо 200"
 
 
 @pytest.mark.smoke
 def test_register_and_delete_user():
     status = pet.register_user_and_delete()
-    assert status == 200, f"Ожидался статус 200, но получен статус {status}"
+    assert status == 200, f"Получен статус {status} вместо 200"
 
 
 @pytest.mark.xfail
 @pytest.mark.regression
 def test_register_user_with_incorrect_emails():
     status = pet.register_user_with_incorrect_emails()
-    assert status == 422, f"Ожидался статус 422, но получен статус {status}"
+    assert status == 422, f"Получен статус {status} вместо 200"
 
 
 @pytest.mark.regression
 def test_register_user_without_confirm_password():
     status = pet.register_user_without_confirm_password()
-    assert status == 422, f"Ожидался статус 422, но получен статус {status}"
+    assert status == 422, f"Получен статус {status} вместо 200"
 
 
 @pytest.mark.regression
 def test_register_user_with_inv_passwords():
     status, returned_response = pet.register_user_with_inv_passwords()
-    assert status == 400, f'Ожидался статус 400, но получен статус {status}'
+    assert status == 400, f'Получен статус {status} вместо 200'
     assert returned_response == 'Username is taken or pass issue'
 
 
 @pytest.mark.regression
 def test_login_with_invalid_email():
     status, returned_response = pet.login_with_invalid_email()
-    assert status == 400, f'Ожидался статус 400, но получен статус {status}'
+    assert status == 400, f'Получен статус {status} вместо 400'
     assert returned_response == 'Username is taken or pass issue'
 
 
 @pytest.mark.regression
 def test_login_with_invalid_password():
     status, returned_response = pet.login_with_invalid_password()
-    assert status == 400, f'Ожидался статус 400, но получен статус {status}'
+    assert status == 400, f'Получен статус {status} вместо 400'
     assert returned_response == 'Username is taken or pass issue'
 
 
 @pytest.mark.smoke
 def test_get_list_users():
     status, user_id = pet.get_list_users()
-    assert status == 200, f'Ожидался статус 200, но получен статус {status}'
+    assert status == 200, f'Получен статус {status} вместо 200'
     assert user_id is not None
 
 
 @pytest.mark.smoke
 def test_add_pet():
     status, pet_id = pet.add_pet()
-    assert status == 200, f'Ожидался статус 200, но получен статус {status}'
+    assert status == 200, f'Получен статус {status} вместо 200'
     assert pet_id is not None
 
 
 @pytest.mark.regression
 def test_dd_pet_without_req_data():
     status = pet.add_pet_without_req_data()
-    assert status == 422, f'Ожидался статус 422, но получен статус {status}'
+    assert status == 422, f'Получен статус {status} вместо 422'
 
 
 @pytest.mark.smoke
@@ -85,14 +85,14 @@ def test_get_pet():
     status = pet.get_pet()[0]
     pet_data = pet.get_pet()[1]
     assert status == 200
-    assert isinstance(pet_data, dict), "Полученные данные не соответствуют ожидаемой структуре"
+    assert isinstance(pet_data, dict), "Данные не соответствуют ожидаемой структуре"
     assert all(key in pet_data for key in EXPECTED_PET_DATA)
 
 
 @pytest.mark.smoke
 def test_add_pet_photo():
     status, link = pet.add_pet_photo()
-    assert status == 200, f'Ожидался статус 200, но получен статус {status}'
+    assert status == 200, f'Получен статус {status} вместо 200'
     assert link
 
 
@@ -100,7 +100,7 @@ def test_add_pet_photo():
 def test_eit_pet():
     status = pet.edit_pet()[0]
     updated_pet_data = pet.edit_pet()[2]
-    assert status == 200, f'Ожидался статус 200, но получен статус {status}'
+    assert status == 200, f'Получен статус {status} вместо 200'
     assert updated_pet_data["pet"]["name"] == 'NewMolly', 'Не удалось изменить имя питомца'
     assert updated_pet_data["pet"]["type"] == 'cat', 'Не удалось изменить тип питомца'
     assert updated_pet_data["pet"]["age"] == 7, 'Не удалось изменить возраст питомца'
@@ -110,7 +110,7 @@ def test_eit_pet():
 @pytest.mark.smoke
 def test_like_my_pet():
     status, current_pet_id, initially_like_count, current_likes_count = pet.like_my_pet()
-    assert status == 200, f'Ожидался статус 200, но получен статус {status}'
+    assert status == 200, f'Получен статус {status} вместо 200'
     assert current_pet_id is not None
     assert initially_like_count != current_likes_count
 
@@ -118,7 +118,7 @@ def test_like_my_pet():
 # @pytest.mark.regression
 # def test_like_not_my_pet():
 #     status, current_pet_id, initially_like_count, current_likes_count = pet.like_not_my_pet()
-#     assert status == 200, f'Ожидался статус 200, но получен статус {status}'
+#     assert status == 200, f'Получен статус {status} вместо 200'
 #     assert current_pet_id is not None
 #     assert initially_like_count != current_likes_count
 
